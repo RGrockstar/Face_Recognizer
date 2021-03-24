490dcdc332a6cbdf97a17f52ded378d6ab53658d
RGrockstar <74416609+RGrockstar@users.noreply.github.com>
2021-03-24 13:09:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+Webcam.set({
+ width:350,
+ height:350,
+ image_format:'png',
+ png_quality:90
+});
+    
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snap(){
+ Webcam.snap(function(data_uri){
+  document.getElementById("result").innerHTML='<img id="CaptureImage" src="'+data_uri+'"/>';
+})
+}
+console.log('ml5 version: ',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/train/image/1yWzwyiVVpN5WcGjqayWSX3yKik2ycIqd',modelLoaded);
